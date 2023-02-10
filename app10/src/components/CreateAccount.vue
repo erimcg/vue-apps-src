e576dd5310f8cae62c29356ba0f21bfce9c376bd
Eric McGregor <eric@n0code.net>
2023-02-10 15:34:01 -0500

    added ref function and other experiments

--- app10/src/components/CreateAccount.vue
+++ app10/src/components/CreateAccount.vue
@@ -1,6 +1,6 @@
 <script setup>
     import { ref, reactive, watch } from 'vue'
-    import { onMounted, onUpdated} from 'vue'
+    import { onMounted, onUpdated } from 'vue'
 
     const usernameRef = ref(null)
 
@@ -17,36 +17,60 @@
     const passwordErrorRef = ref(null)
     const password2ErrorRef = ref(null)
 
+    // watch element ref - similar to onMounted - not watching value of input
+    // for some reason disabled when ref function is installed 
+    let count = 0
+    watch(usernameRef, () => {
+        console.log('watching usernameRef')
+        console.log(usernameRef.value)
+        console.log(count++)
+        usernameRef.value.value = "rmcgregor"
+    })
 
+    // called when anything in component changes - including onMounted
+    // if used in input element, must have v-model set
+    function componentUpdate(elm) {
+        console.log("password changed")
+        console.log(elm.value)
+        console.log(passwordErrorRef.value)
+
+        if (passwordErrorRef.value != null && elm.value.length > 0) {
+
+            validPassword.value = (elm.value.length >= 8)
+
+            passwordErrorRef.value.innerHTML = (validPassword.value) ?
+                "&nbsp;" : "Minimum length: 8 characters"
+        }
+    }
+
+    //watch v-model input.value ref (called when string changes)
     watch(username, () => {
         validUsername.value = (username.value.length >= 4)
 
         usernameErrorRef.value.innerHTML = (validUsername.value) ?
             "&nbsp;" : "Minimum length: 4 characters"
+
+        usernameRef.value.setAttribute("data", "hello")
     })
 
-    watch(password, () => {
-        validPassword.value = (password.value.length >= 8)
-
-        passwordErrorRef.value.innerHTML = (validPassword.value) ?
-            "&nbsp;" : "Minimum length: 4 characters"
-    })
-
-    watch(password2, () => {
+     // watch array of refs
+    watch([password, password2], () => {
         passwordsMatch.value = (password.value === password2.value)
 
-        password2ErrorRef.value.innerHTML = (passwordsMatch.value === true) ?
+        password2ErrorRef.value.innerHTML = (passwordsMatch.value || password2.value.length == 0) ?
             "&nbsp;" : "Passwords do not match"
 
     })
 
+    // watch array of refs
     watch([validUsername, validPassword, passwordsMatch], async () => {
         disabled.value = !(validUsername.value && validPassword.value && passwordsMatch.value)
         console.log(disabled.value)
+
+        if (!disabled.value) usernameRef.value.value = ''
     })
 
     function submit() {
-        
         let mssg = username.value + ", " + password.value
         console.log(mssg)
     }
@@ -56,7 +80,7 @@
     })
 
     onUpdated(() => {
-        console.log(usernameRef.value.value)
+        //console.log(usernameRef.value.value)
     })
 
 </script>
@@ -68,26 +92,28 @@
             <fieldset class="d-flex flex-column">
                 <legend>Create Account</legend>
 
-                <div class="form-floating">
+                <div class="form-floating mb-2">
                     <input ref="usernameRef" v-model="username" id="username" type="text" class="form-control" />
                     <label for="username" class="form-label">Username</label>
-                    <span ref="usernameErrorRef">&nbsp</span>
+                    <small ref="usernameErrorRef" class="text-danger">&nbsp</small>
                 </div>
 
-                <div class="form-floating">
-                    <input v-model="password" id="password" type="password" class="form-control" />
+                <div class="form-floating mb-2">
+                    <input :ref="componentUpdate" v-model="password" id="password" type="password"
+                        class="form-control" />
                     <label for="password" class="form-label">Password</label>
-                    <span ref="passwordErrorRef">&nbsp;</span>
+                    <small ref="passwordErrorRef" class="text-danger">&nbsp;</small>
                 </div>
 
-                <div class="form-floating">
+                <div class="form-floating mb-2">
                     <input v-model="password2" id="password2" type="password" class="form-control" />
                     <label for="password2" class="form-label">Reenter password</label>
-                    <span ref="password2ErrorRef">&nbsp;</span>
+                    <small ref="password2ErrorRef" class="text-danger">&nbsp;</small>
                 </div>
 
                 <div>
-                    <button @click="submit" class="btn btn-primary" type="button" v-bind:disabled="disabled">Create</button>
+                    <button @click="submit" class="btn btn-primary" type="button"
+                        v-bind:disabled="disabled">Create</button>
                 </div>
             </fieldset>
         </form>
@@ -96,9 +122,5 @@
 </template>
 
 <style>
-span {
-    font-size: small;
-    color: red;
-    vertical-align: text-top;
-}
+
 </style>
