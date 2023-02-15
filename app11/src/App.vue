0806cdbe70d68fb7447ff047e77373fb7d034a17
Eric McGregor <eric@n0code.net>
2023-02-14 19:23:34 -0500

    added vue logo

--- app11/src/App.vue
+++ app11/src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+    import Logo from './components/Logo.vue'
+
     import RefExp1 from './components/RefExp1.vue'
     import RefExp2 from './components/RefExp2.vue'
     import RefExp3 from './components/RefExp3.vue'
@@ -6,6 +8,7 @@
 </script>
 
 <template>
+    <Logo />
     <div class="container mt-5">
         <h3>Watch Experiments</h3>
         <RefExp1 />
---
+++ app11/src/components/Logo.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div id="logo">
+    <div id="title">Vue</div>
+    <div id="subtitle">Ref Experiments</div>
+    <img alt="Vue logo" src="../assets/vueLogo.png" />
+  </div>
+</template>
+
+<style>
+#logo {
+  margin: 15px;
+  
+  aspect-ratio: 1 / 1;
+  width: 15vh;
+
+  background-color: lightgray;
+  border-radius: 100px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+#title {
+  font-size: 2vh;
+}
+
+#subtitle {
+  font-size: 1.2vh;
+}
+
+img {
+  display: inline-block;
+  width: 50%;
+}
+</style>
